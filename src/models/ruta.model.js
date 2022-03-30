ff26a4b66c0e53cdc64ab5ef23bfbd841d075ea9
Alex Enciso <alexpenciso@gmail.com>
2022-03-29 21:43:01 -0400

    Creado model para rutas

---
+++ src/models/ruta.model.js
@@ -0,0 +1,23 @@
+const {DataTypes} = require('sequelize');
+const {sequelize} = require('../services/bd.service');
+
+const RutaModel = sequelize.define('Ruta', {
+  // Model attributes are defined here
+  rut_codigo: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  rut_descripcion: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+}, {
+  tableName: 'ruta',
+  timestamps: false,
+});
+
+module.exports = {
+  RutaModel,
+};
