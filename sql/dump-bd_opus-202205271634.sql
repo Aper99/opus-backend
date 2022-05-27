11b8eb1d87e0286a24518e34bfccd0a11e614c0d
Alex Enciso <alexpenciso@gmail.com>
2022-05-27 16:37:17 -0400

    Agregado nuevo backup de sql

--- sql/dump-bd_opus-202205271634.sql
+++ sql/dump-bd_opus-202205271634.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.10
 -- Dumped by pg_dump version 12.10
 
--- Started on 2022-04-07 21:12:56
+-- Started on 2022-05-27 16:34:41
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -29,7 +29,7 @@ CREATE SCHEMA public;
 ALTER SCHEMA public OWNER TO postgres;
 
 --
--- TOC entry 2888 (class 0 OID 0)
+-- TOC entry 2900 (class 0 OID 0)
 -- Dependencies: 3
 -- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
 --
@@ -76,7 +76,7 @@ CREATE SEQUENCE public.cliente_cli_codigo_seq
 ALTER TABLE public.cliente_cli_codigo_seq OWNER TO postgres;
 
 --
--- TOC entry 2889 (class 0 OID 0)
+-- TOC entry 2901 (class 0 OID 0)
 -- Dependencies: 202
 -- Name: cliente_cli_codigo_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -118,7 +118,7 @@ CREATE SEQUENCE public.empleado_emp_codigo_seq
 ALTER TABLE public.empleado_emp_codigo_seq OWNER TO postgres;
 
 --
--- TOC entry 2890 (class 0 OID 0)
+-- TOC entry 2902 (class 0 OID 0)
 -- Dependencies: 208
 -- Name: empleado_emp_codigo_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -156,7 +156,7 @@ CREATE SEQUENCE public.ruta_rut_codigo_seq
 ALTER TABLE public.ruta_rut_codigo_seq OWNER TO postgres;
 
 --
--- TOC entry 2891 (class 0 OID 0)
+-- TOC entry 2903 (class 0 OID 0)
 -- Dependencies: 204
 -- Name: ruta_rut_codigo_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -194,7 +194,7 @@ CREATE SEQUENCE public.sistema_sis_codigo_seq
 ALTER TABLE public.sistema_sis_codigo_seq OWNER TO postgres;
 
 --
--- TOC entry 2892 (class 0 OID 0)
+-- TOC entry 2904 (class 0 OID 0)
 -- Dependencies: 206
 -- Name: sistema_sis_codigo_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -238,12 +238,53 @@ CREATE SEQUENCE public.tarea_tra_numero_seq
 ALTER TABLE public.tarea_tra_numero_seq OWNER TO postgres;
 
 --
--- TOC entry 2893 (class 0 OID 0)
+-- TOC entry 2905 (class 0 OID 0)
 -- Dependencies: 210
 -- Name: tarea_tra_numero_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE public.tarea_tra_numero_seq OWNED BY public.tarea.tra_numero;
+
+
+--
+-- TOC entry 215 (class 1259 OID 16490)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name character varying,
+    token character varying,
+    passwd character varying,
+    email character varying
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 214 (class 1259 OID 16488)
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 2906 (class 0 OID 0)
+-- Dependencies: 214
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
@@ -278,7 +319,7 @@ CREATE SEQUENCE public.usuario_usu_codigo_seq
 ALTER TABLE public.usuario_usu_codigo_seq OWNER TO postgres;
 
 --
--- TOC entry 2894 (class 0 OID 0)
+-- TOC entry 2907 (class 0 OID 0)
 -- Dependencies: 213
 -- Name: usuario_usu_codigo_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -287,7 +328,7 @@ ALTER SEQUENCE public.usuario_usu_codigo_seq OWNED BY public.usuario.usu_codigo;
 
 
 --
--- TOC entry 2723 (class 2604 OID 16399)
+-- TOC entry 2730 (class 2604 OID 16399)
 -- Name: cliente cli_codigo; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -295,7 +336,7 @@ ALTER TABLE ONLY public.cliente ALTER COLUMN cli_codigo SET DEFAULT nextval('pub
 
 
 --
--- TOC entry 2726 (class 2604 OID 16437)
+-- TOC entry 2733 (class 2604 OID 16437)
 -- Name: empleado emp_codigo; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -303,7 +344,7 @@ ALTER TABLE ONLY public.empleado ALTER COLUMN emp_codigo SET DEFAULT nextval('pu
 
 
 --
--- TOC entry 2724 (class 2604 OID 16410)
+-- TOC entry 2731 (class 2604 OID 16410)
 -- Name: ruta rut_codigo; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -311,7 +352,7 @@ ALTER TABLE ONLY public.ruta ALTER COLUMN rut_codigo SET DEFAULT nextval('public
 
 
 --
--- TOC entry 2725 (class 2604 OID 16426)
+-- TOC entry 2732 (class 2604 OID 16426)
 -- Name: sistema sis_codigo; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -319,7 +360,7 @@ ALTER TABLE ONLY public.sistema ALTER COLUMN sis_codigo SET DEFAULT nextval('pub
 
 
 --
--- TOC entry 2727 (class 2604 OID 16448)
+-- TOC entry 2734 (class 2604 OID 16448)
 -- Name: tarea tra_numero; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -327,7 +368,15 @@ ALTER TABLE ONLY public.tarea ALTER COLUMN tra_numero SET DEFAULT nextval('publi
 
 
 --
--- TOC entry 2728 (class 2604 OID 16474)
+-- TOC entry 2736 (class 2604 OID 16493)
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- TOC entry 2735 (class 2604 OID 16474)
 -- Name: usuario usu_codigo; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -335,46 +384,51 @@ ALTER TABLE ONLY public.usuario ALTER COLUMN usu_codigo SET DEFAULT nextval('pub
 
 
 --
--- TOC entry 2872 (class 0 OID 16396)
+-- TOC entry 2882 (class 0 OID 16396)
 -- Dependencies: 203
 -- Data for Name: cliente; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.cliente (cli_codigo, cli_nombre, cli_rucci, cli_telefono, cli_email, cli_direccion, cli_codrut) FROM stdin;
-11	Alex	4761137	0973620234	alexpenciso@gmail.com	algun lado	3
-7	Marta	123456	98745631	maria@gmail.com	Pablo Rojas	1
-1	Jose Carlos	12345645	9874563145	jose@gmail.com	Pablo Rojas 	1
-12	Alicia	47994156	0973895452	alicia@hotmail.com	algun lado	3
+1	Despensa Jose Carlos	12345645	9874563145	jose@gmail.com	Pablo Rojas 	4
+7	Comercial Marta	123456	98745631	maria@gmail.com	Pablo Rojas	1
+11	Supermercado 8	4761137	0973620234	alexpenciso@gmail.com	algun lado	3
+12	Libreria Greta	47994156	0973895452	alicia@hotmail.com	algun lado	3
+15	Heladeria Miguel	678646				4
+16	Cliente Nuevo	57575	5575		centro	6
+17	Jose	4587687	678676		km 9	1
 \.
 
 
 --
--- TOC entry 2878 (class 0 OID 16434)
+-- TOC entry 2888 (class 0 OID 16434)
 -- Dependencies: 209
 -- Data for Name: empleado; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.empleado (emp_codigo, emp_nombre, emp_telefono, emp_ci, emp_direccion, emp_codusu) FROM stdin;
 1	Gael L	0976854746	46514894	Centro	1
-5	Marta	0975466	45864	San Juan	2
+5	Marta	0975466564	45864789	San Juan 2	17
+8	Alex	0973620245	4761137	San Isidro	15
 \.
 
 
 --
--- TOC entry 2874 (class 0 OID 16407)
+-- TOC entry 2884 (class 0 OID 16407)
 -- Dependencies: 205
 -- Data for Name: ruta; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.ruta (rut_codigo, rut_descripcion) FROM stdin;
-1	Ruta 1
 3	Ruta 5
 4	Ruta 10
+1	Ruta 9
+6	Ruta Centro
 \.
 
 
 --
--- TOC entry 2876 (class 0 OID 16423)
+-- TOC entry 2886 (class 0 OID 16423)
 -- Dependencies: 207
 -- Data for Name: sistema; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -387,19 +441,38 @@ COPY public.sistema (sis_codigo, sis_descripcion) FROM stdin;
 
 
 --
--- TOC entry 2880 (class 0 OID 16445)
+-- TOC entry 2890 (class 0 OID 16445)
 -- Dependencies: 211
 -- Data for Name: tarea; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.tarea (tra_numero, tra_fecha, tra_codsis, tra_codcli, tra_codemp, tra_descripcion, tra_estado, tra_obs) FROM stdin;
-1	2022-01-01 15:00:00	1	1	1	Instalar la nueva impresora	F	Se instalo sin problemas la impreso
-2	2023-01-01 15:00:00	1	1	1	Instalar la nueva computadora	A	
+3	2022-01-01 15:00:00	1	1	1	Instalar la nueva impresora 2	F	Se instalo sin problemas la impreso
+11	2022-05-27 18:28:00	6	11	5	limpieza de pc	I	\N
+10	2022-05-27 22:14:00	3	7	5	hacer visita tecnica	F	se realizo la visita tecnica
+2	2023-01-01 18:00:00	1	1	8	Instalar la nueva computadora, ver si hace falta comprar tones\npasar presupuesto	A	pausado
+5	2022-01-01 15:00:00	3	7	8	Instalar la nueva impresora 2	F	Se instalo sin problemas la impreso
+13	2022-05-27 20:06:00	1	1	8	instalar monitor nuevo	F	se instalo el nuevo monitor
+15	2022-05-28 00:22:00	3	12	8	ir a instalar la impresora 	F	se instalo la nueva impresora sin problemas
 \.
 
 
 --
--- TOC entry 2881 (class 0 OID 16469)
+-- TOC entry 2894 (class 0 OID 16490)
+-- Dependencies: 215
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, name, token, passwd, email) FROM stdin;
+17	Perla	\N	179	perla@gmail.com
+15	Alex Paul	\N	123	alexpenciso@gmail.com
+14	Otra empresa	\N	9874651	marta@gmail.com
+16	ana	\N	12345	ana@gmail.com
+\.
+
+
+--
+-- TOC entry 2891 (class 0 OID 16469)
 -- Dependencies: 212
 -- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -410,52 +483,61 @@ COPY public.usuario (usu_codigo, usu_email, usu_password, usu_nombre) FROM stdin
 
 
 --
--- TOC entry 2895 (class 0 OID 0)
+-- TOC entry 2908 (class 0 OID 0)
 -- Dependencies: 202
 -- Name: cliente_cli_codigo_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.cliente_cli_codigo_seq', 13, true);
+SELECT pg_catalog.setval('public.cliente_cli_codigo_seq', 17, true);
 
 
 --
--- TOC entry 2896 (class 0 OID 0)
+-- TOC entry 2909 (class 0 OID 0)
 -- Dependencies: 208
 -- Name: empleado_emp_codigo_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.empleado_emp_codigo_seq', 6, true);
+SELECT pg_catalog.setval('public.empleado_emp_codigo_seq', 10, true);
 
 
 --
--- TOC entry 2897 (class 0 OID 0)
+-- TOC entry 2910 (class 0 OID 0)
 -- Dependencies: 204
 -- Name: ruta_rut_codigo_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.ruta_rut_codigo_seq', 4, true);
+SELECT pg_catalog.setval('public.ruta_rut_codigo_seq', 7, true);
 
 
 --
--- TOC entry 2898 (class 0 OID 0)
+-- TOC entry 2911 (class 0 OID 0)
 -- Dependencies: 206
 -- Name: sistema_sis_codigo_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.sistema_sis_codigo_seq', 7, true);
+SELECT pg_catalog.setval('public.sistema_sis_codigo_seq', 8, true);
 
 
 --
--- TOC entry 2899 (class 0 OID 0)
+-- TOC entry 2912 (class 0 OID 0)
 -- Dependencies: 210
 -- Name: tarea_tra_numero_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.tarea_tra_numero_seq', 2, true);
+SELECT pg_catalog.setval('public.tarea_tra_numero_seq', 15, true);
 
 
 --
--- TOC entry 2900 (class 0 OID 0)
+-- TOC entry 2913 (class 0 OID 0)
+-- Dependencies: 214
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 20, true);
+
+
+--
+-- TOC entry 2914 (class 0 OID 0)
 -- Dependencies: 213
 -- Name: usuario_usu_codigo_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -464,7 +546,7 @@ SELECT pg_catalog.setval('public.usuario_usu_codigo_seq', 2, true);
 
 
 --
--- TOC entry 2730 (class 2606 OID 16404)
+-- TOC entry 2738 (class 2606 OID 16404)
 -- Name: cliente cliente_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -473,7 +555,7 @@ ALTER TABLE ONLY public.cliente
 
 
 --
--- TOC entry 2736 (class 2606 OID 16442)
+-- TOC entry 2744 (class 2606 OID 16442)
 -- Name: empleado empleado_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -482,7 +564,7 @@ ALTER TABLE ONLY public.empleado
 
 
 --
--- TOC entry 2732 (class 2606 OID 16415)
+-- TOC entry 2740 (class 2606 OID 16415)
 -- Name: ruta ruta_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -491,7 +573,7 @@ ALTER TABLE ONLY public.ruta
 
 
 --
--- TOC entry 2734 (class 2606 OID 16431)
+-- TOC entry 2742 (class 2606 OID 16431)
 -- Name: sistema sistema_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -500,7 +582,7 @@ ALTER TABLE ONLY public.sistema
 
 
 --
--- TOC entry 2738 (class 2606 OID 16453)
+-- TOC entry 2746 (class 2606 OID 16453)
 -- Name: tarea tarea_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -509,7 +591,16 @@ ALTER TABLE ONLY public.tarea
 
 
 --
--- TOC entry 2740 (class 2606 OID 16482)
+-- TOC entry 2750 (class 2606 OID 16498)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2748 (class 2606 OID 16482)
 -- Name: usuario usuario_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -518,7 +609,7 @@ ALTER TABLE ONLY public.usuario
 
 
 --
--- TOC entry 2741 (class 2606 OID 16416)
+-- TOC entry 2751 (class 2606 OID 16416)
 -- Name: cliente cliente_cli_codrut_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -527,7 +618,7 @@ ALTER TABLE ONLY public.cliente
 
 
 --
--- TOC entry 2743 (class 2606 OID 16459)
+-- TOC entry 2753 (class 2606 OID 16459)
 -- Name: tarea tarea_tra_codcli_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -536,7 +627,7 @@ ALTER TABLE ONLY public.tarea
 
 
 --
--- TOC entry 2744 (class 2606 OID 16464)
+-- TOC entry 2754 (class 2606 OID 16464)
 -- Name: tarea tarea_tra_codemp_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -545,7 +636,7 @@ ALTER TABLE ONLY public.tarea
 
 
 --
--- TOC entry 2742 (class 2606 OID 16454)
+-- TOC entry 2752 (class 2606 OID 16454)
 -- Name: tarea tarea_tra_codsis_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -553,7 +644,7 @@ ALTER TABLE ONLY public.tarea
     ADD CONSTRAINT tarea_tra_codsis_fkey FOREIGN KEY (tra_codsis) REFERENCES public.sistema(sis_codigo) NOT VALID;
 
 
--- Completed on 2022-04-07 21:12:57
+-- Completed on 2022-05-27 16:34:41
 
 --
 -- PostgreSQL database dump complete
